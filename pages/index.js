e9ba44b5cfafff294da074dc2d6fb6dda90aa4c9
Felix Sebastian <hi@felixsebastian.dev>
2022-05-13 14:58:02 +1000

    add redirect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  useEffect(() => { window.location = "https://cultureamp.cultureamp.com/app/continuous-reporting-app" }, []);
   return (
     <div className="container">
       <Head>
@@ -11,10 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="This site is no longer available. Redirecting you to the new URL now" />
       </main>
 
       <Footer />
