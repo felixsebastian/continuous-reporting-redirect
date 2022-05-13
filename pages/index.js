0447d56abdd5e6f0018ca7f6381559711a592f04
Felix Sebastian <hi@felixsebastian.dev>
2022-05-13 14:58:42 +1000

    update title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Page not found</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
